bc57d79714de592cfc79a9adab80de874a9adc29
VamshiKrishna <vamshikrishna1996016@gmail.com>
2024-10-01 12:46:01 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,9 +1,45 @@
-const heading = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag")
-    ])
+import React, {createElement} from "react";
+import {createRoot} from "react-dom/client";
+
+// React.createElement => converted into (React Element - JS Object) = (rendered as html element)HTML Element (render)
+
+//core react
+// const heading = createElement("div",{id:"parent"},
+//     createElement("div",{id:"child"},[
+//        createElement("h1",{},"I am h1 tag"),
+//         createElement("h2",{},"I am h2 tag")
+//     ])
+// );
+// console.log(heading); //object
+
+//JSx => convertrf to React Element using babel(JS compiler) - JS Object => HTML Element (render)
+// const jsxHeading = <h1>Hello JSX !</h1>
+// console.log(jsxHeading); //object
+
+// const root = createRoot(document.getElementById("root"));
+// root.render(heading); //it will everything inside root with heading it wont append
+
+const arr = [1,2,3];
+const elem = <span>React Element !</span>
+//normal js variable
+const title = (
+  <div>
+    <h1>Name React 1</h1>
+    {elem} // putting react element inside another react element
+    <p>I am title paragraph</p>
+  </div>
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading) //object
-root.render(heading);
+const Title = () => {return <h1>Namste React !</h1>};
+const Heading = () => (
+    <div>
+            {
+                arr.map(ele => console.log(ele))
+            }
+            {title}
+            {Title()}
+            <Title />
+            <h1>Heading Component !</h1>
+    </div>
+);
+const root = createRoot(document.getElementById("root"));
+root.render(<Heading />); 
--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 This is my repo for react project.
 <br/>
 Author - Vamshi (React demo project)
+
+# parcel
+- It has created a dev build.
+- It has created a local server.
+- It automatically refresh the page after saving i.e HMR = Hot Module Replacement.
+- File watching algorithm - written in c++
+- caching - Faster builds
+- image optimization
+- minifiy files - minification
+- bundling
+- compress all your files - compressing
+- consitent hashing
+- code splitting
+- differential bundling = to suuport older browers
+- diagnostic
+- error handling
+- it also helps you host your app in Https
+- tree shaking
+- different dev and prod builds
+
